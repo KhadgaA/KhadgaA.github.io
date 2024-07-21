0920e1dec1e539cab02f2aac7192db0fe72665e0
KhadgaA <88937070+KhadgaA@users.noreply.github.com>
2024-07-21 18:03:41 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -26,13 +26,20 @@ const CONFIG = {
           title: 'Multilingual Speaker Identification and Verification System',
           description: 'Developed a system achieving 98% accuracy in identification across 5 Indian languages.',
           imageUrl: 'https://example.com/speaker_recognition.jpg', // Replace with actual image URL
-          link: 'https://github.com/KhadgaA/speaker-recognition', // Replace with actual project link
+          link: 'https://github.com/KhadgaA/Multilingual-Speaker-Verification', // Replace with actual project link
         },
         {
           title: 'VQ-VAE and Transformer-based Image Synthesis for Skin Lesion Analysis',
           description: 'Implemented VQ-VAE for skin lesion image synthesis with PSNR value of 12.7.',
           imageUrl: 'https://example.com/skin_lesion.jpg', // Replace with actual image URL
-          link: 'https://github.com/KhadgaA/skin-lesion-synthesis', // Replace with actual project link
+          link: 'https://github.com/KhadgaA/VQ-VAE', // Replace with actual project link
+        },
+        
+        {
+          title: 'Advanced Eye Disease Detection using Knowledge Distillation and Masked Vision Transformers',
+          description: 'Developed an advanced eye disease detection system using Knowledge Distillation, achieving 84.4% accuracy with Resnet18 student model, approaching the 87.5% accuracy of the Resnet50 teacher model on the ODIR dataset of 7,000 images across 8 disease categories',
+          imageUrl: 'https://example.com/skin_lesion.jpg', // Replace with actual image URL
+          link: 'https://github.com/KhadgaA/Masked-Autoencoders-for-Eye-Disease-Detection', // Replace with actual project link
         },
       ],
     },
@@ -48,12 +55,13 @@ const CONFIG = {
     email: 'khadgajyothalli@gmail.com',
   },
   resume: {
-    fileUrl: 'https://example.com/your-resume.pdf', // Replace with your actual resume URL
+    fileUrl: 'https://github.com/KhadgaA/KhadgaA/blob/main/Khadga_Resume.pdf', // Replace with your actual resume URL
   },
   skills: [
     'Python',
     'PyTorch',
     'Scikit-Learn',
+    'OpenCV',
     'Deep Learning',
     'Computer Vision',
     'NLP',
@@ -74,6 +82,13 @@ const CONFIG = {
       position: 'Teaching Assistant',
       from: 'August 2023',
       to: 'December 2023',
+      companyLink: 'https://iitj.ac.in/',
+    },
+    {
+      company: 'Indian Institute of Technology, Jodhpur',
+      position: 'Summer Intern',
+      from: 'May 2022',
+      to: 'July 2022',
       companyLink: 'https://iitj.ac.in/',
     },
   ],
